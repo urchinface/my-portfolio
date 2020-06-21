ce335992bac8cacbc5c6747b54f08abd44ac2a8a
Sophia <sophiastaves@gmail.com>
2020-06-21 18:22:19 +0100

    Updates

---
+++ precache-manifest.ee1e6d2c2db811240eadd2ce22d9477f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3629ac07565a7ef986fe1c2a64049bb",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "fc947b5ed9fb89738ae8",
+    "url": "/my-portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "11235602ea0c1ce0a0da",
+    "url": "/my-portfolio/static/js/2.04e7d5bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.04e7d5bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc947b5ed9fb89738ae8",
+    "url": "/my-portfolio/static/js/main.2408e031.chunk.js"
+  },
+  {
+    "revision": "b53c4e8aaf29ecf343d7",
+    "url": "/my-portfolio/static/js/runtime-main.4a0bf746.js"
+  }
+]);
